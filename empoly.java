ccc16ef7a354c9b789308b15b09008437fec4f14
liban32 <libankalif46@gmail.com>
2024-09-04 11:35:36 +0300

    Update empoly.java

---
+++ empoly.java
@@ -0,0 +1,9 @@
+public class man 
+{
+public static void main(String[]args)
+{
+ int wage= 5000;
+ String wage_String = Integer.toString(wage);
+ System.out.println("wage as a String is"+ " " + wage_String);
+}
+}
