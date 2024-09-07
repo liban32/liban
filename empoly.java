d21d0ea62467e8b0b03501f00c4927335e95b36f
liban32 <libankalif46@gmail.com>
2024-09-07 11:13:25 +0300

    Delete empoly.java

--- empoly.java
+++
@@ -1,9 +0,0 @@
-public class man 
-{
-public static void main(String[]args)
-{
- int wage= 5000;
- String wage_String = Integer.toString(wage);
- System.out.println("wage as a String is"+ " " + wage_String);
-}
-}
